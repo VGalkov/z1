89acd817bb0d9bfc26087bc23a7f6dc07b013fb5
Vladimir Galkov <s0506777@yandex.ru>
2023-01-04 21:14:17 +0400

    болванка для собеседований maven
    для параметризации... просто для галочки. объяснение из пальца.

---
+++ src/main/java/org/example/helpClasses/ParametrisedClass.java
@@ -0,0 +1,33 @@
+package org.example.helpClasses;
+
+public class ParametrisedClass {
+
+    private Boolean a;
+    private String b;
+
+    public ParametrisedClass(Boolean a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public boolean isA() {
+        return a;
+    }
+
+    public void setA(Boolean a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+
+    public <T1> Object returnMe(T1 obj) {
+        return obj;
+    }
+}
